6b353de71e4f20fd241b2476146c47d8fcfd8c7c
ypw <915505626@qq.com>
2015-01-15 01:20:17 +0800

    acceleration

---
+++ gen/com/ypw/acceleration/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ypw.acceleration;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/hk/ypw/acceleration/settings.java
@@ -0,0 +1,68 @@
+package hk.ypw.acceleration;
+
+import com.ypw.acceleration.R;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class settings extends Activity{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        
+		EditText acc = (EditText)findViewById(R.id.edit_acc);
+		EditText gps = (EditText)findViewById(R.id.edit_gps);
+		EditText gyro = (EditText)findViewById(R.id.edit_gyro);
+		EditText mag = (EditText)findViewById(R.id.edit_mag);
+		EditText filename = (EditText)findViewById(R.id.edit_filename);
+
+		
+		try{
+			SharedPreferences sp = getSharedPreferences("data", 0);
+			String accString=sp.getString("acc", "1000");
+			String gpsString=sp.getString("gps", "3000");
+			String gyroString=sp.getString("gyro", "1000");
+			String magString=sp.getString("mag", "1000");
+			String filenameString=sp.getString("filename", "output.txt");
+			acc.setText(accString);
+			gps.setText(gpsString);
+			gyro.setText(gyroString);
+			mag.setText(magString);
+			filename.setText(filenameString);
+			
+		}catch(Exception e)
+		{}
+	}
+	public void save(View v)
+	{
+		SharedPreferences.Editor sp = getSharedPreferences("data", 0).edit();
+        
+		EditText acc = (EditText)findViewById(R.id.edit_acc);
+		EditText gps = (EditText)findViewById(R.id.edit_gps);
+		EditText gyro = (EditText)findViewById(R.id.edit_gyro);
+		EditText mag = (EditText)findViewById(R.id.edit_mag);
+		EditText filename = (EditText)findViewById(R.id.edit_filename);
+
+		String accString = acc.getText().toString();
+		String gpsString = gps.getText().toString();
+		String gyroString = gyro.getText().toString();
+		String magString = mag.getText().toString();
+		String filenameString = filename.getText().toString();
+		
+		sp.putString("acc",	accString);
+		sp.putString("gps",	gpsString);
+		sp.putString("gyro",gyroString);
+		sp.putString("mag",	magString);
+		sp.putString("filename",filenameString);
+		sp.commit();
+		
+		Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
+		finish();
+	}
+}
